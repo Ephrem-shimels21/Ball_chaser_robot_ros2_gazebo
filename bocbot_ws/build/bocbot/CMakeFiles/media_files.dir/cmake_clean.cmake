6f27b3f3ba5989f5e55cdacd1e413dbd6f3c3d06
Ephrem-shimels21 <ephrem21shimels@gmail.com>
2024-02-06 13:28:40 +0300

    design the ball chaser robot

---
+++ bocbot_ws/build/bocbot/CMakeFiles/media_files.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "/home/ephrem/bocbot_ws/src/bocbot/urdf/bocbot.urdf"
+  "CMakeFiles/media_files"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/media_files.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
